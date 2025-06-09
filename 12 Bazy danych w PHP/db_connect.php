14f23977c8f328fc1de8f597398be78d084f0977
Uczen 1 <teraz@wp.pl>
2025-06-09 11:55:46 +0200

    Łączenie z bazą danych.

--- 12 Bazy danych w PHP/db_connect.php
+++ 12 Bazy danych w PHP/db_connect.php
@@ -8,7 +8,7 @@ $mysqli = new mysqli($dbServer,$dbUser,$dbPassword,$dbName);
 if(mysqli_connect_errno()){
 	echo 'Błąd bazy danych';
 }
-
+?>
 // w html
 <?php include('db_connect.php'); 
 $result = $mysqli->query("SELECT * FROM articles order by id");
