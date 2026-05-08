422f436565664b940c2d62e06613c948f72e619b
Uczen 1 <teraz@wp.pl>
2026-05-08 09:03:17 +0200

    13-zadanie-do-wyjatkow

---
+++ 72_Petle/T72 - przykłady zastosowania pętli/kod_1.php
@@ -0,0 +1,10 @@
+<?php
+for($i=1; $i<=5; $i++)
+    echo "Pętla wykonana $i raz/y <br>";
+/*
+Pętla wykonana 1 raz/y
+Pętla wykonana 2 raz/y
+Pętla wykonana 3 raz/y
+Pętla wykonana 4 raz/y
+Pętla wykonana 5 raz/y
+ */
